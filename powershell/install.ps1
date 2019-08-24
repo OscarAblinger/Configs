e7c7e14ca4ec3f57e728801dc4f87e4066a26457
Oscar Ablinger <dev.ablinger@gmail.com>
2019-08-24 15:32:20 +0200

    feat(powershell): Add installation script

---
+++ powershell/install.ps1
@@ -0,0 +1,17 @@
+echo This needs access to git
+
+echo ==== Installing fonts
+echo --> Wait for installation to finish
+git clone https://github.com/powerline/fonts.git temp_fonts
+.\temp_fonts\install.ps1
+rm -rf temp_fonts
+
+echo ==== Installing posh-git & oh-my-posh
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name 'posh-git'
+Install-Module -Name 'oh-my-posh'
+
+echo ==== Installing Get-ChildItemColor
+Install-Module -Name 'Get-ChildItemColor'
+
+echo ==== Done. You can now use the config from this repository 
